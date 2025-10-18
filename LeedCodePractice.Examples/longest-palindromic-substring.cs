001a8ee8c4d784e7ce5ae8bcfc31e3792a9c2bb3
Elshod <elshodibadullayev28@gmail.com>
2025-10-19 00:01:47 +0500

    FEATURE: Implement Longest Palindromic Substring algorithm

---
+++ LeedCodePractice.Examples/longest-palindromic-substring.cs
@@ -0,0 +1,28 @@
+﻿namespace LeedCodePractice.Examples
+{
+    public class longest_palindromic_substring
+    {
+        public static int FindLongestPalindromicSubstring(string s)
+        {
+            if (string.IsNullOrEmpty(s)) return 0;
+            int maxLength = 1;
+            for (int i = 0; i < s.Length; i++)
+            {
+                int len1 = ExpandAroundCenter(s, i, i);
+                int len2 = ExpandAroundCenter(s, i, i + 1);
+                int len = Math.Max(len1, len2);
+                maxLength = Math.Max(maxLength, len);
+            }
+            return maxLength;
+        }
+        private static int ExpandAroundCenter(string s, int left, int right)
+        {
+            while (left >= 0 && right < s.Length && s[left] == s[right])
+            {
+                left--;
+                right++;
+            }
+            return right - left - 1;
+        }
+    }
+}
