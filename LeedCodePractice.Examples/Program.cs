3c6d32a16ea9234904e0033b1e24e5d1a26c557a
Elshod <elshodibadullayev28@gmail.com>
2025-10-19 17:14:00 +0500

    FEATURE: Zigzag Conversion

--- LeedCodePractice.Examples/Program.cs
+++ LeedCodePractice.Examples/Program.cs
@@ -7,5 +7,14 @@ public class Program
         string s = "babad";
         int length = longest_palindromic_substring.FindLongestPalindromicSubstring(s);
         Console.WriteLine($"The length of the longest palindromic substring in '{s}' is: {length}");
+
+        ZigzagConversion zigzag = new ZigzagConversion();
+        string converted = zigzag.Convert("PAYPALISHIRING", 3);
+        Console.WriteLine($"The converted string is: {converted}");
+        string converted2 = zigzag.Convert("PAYPALISHIRING", 4);
+        Console.WriteLine($"The converted string is: {converted2}");
+        string converted3 = zigzag.Convert("A", 1);
+        Console.WriteLine($"The converted string is: {converted3}");
+
     }
 }
