535ddf824ae63f1604147c636976a3a6b6e3fb56
Elshod <elshodibadullayev28@gmail.com>
2025-10-20 14:19:41 +0500

    FEATURE: Reverse Words In  String

---
+++ LeedCodePractice.Examples/ReverseWordsInString.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+namespace LeedCodePractice.Examples
+{
+    public class ReverseWordsInString
+    {
+        public string ReverseWords(string s)
+        {
+            string[] words = s.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            var reverseWords = new StringBuilder();
+            for (int i = words.Length-1; i >=0; i--)
+            {
+                reverseWords.Append(words[i]);
+                if (i > 0)
+                    reverseWords.Append(' ');
+            }
+            return reverseWords.ToString();
+        }
+    }
+}
